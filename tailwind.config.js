487d7ba8743f911a188d1896fa25b9d67971beec
Bryan <takz159@mds.com.tw>
2025-07-11 09:23:53 +0800

    更新js檔案

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // <-- 修改成這樣
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
